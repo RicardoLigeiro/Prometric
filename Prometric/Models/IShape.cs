7ba007c41dec9ccb65e118692f9182a10dedd351
Ricardo Ligeiro <ricardo.ligeiro@gmail.com>
2020-10-15 22:25:25 +0100

    Created Ishape

---
+++ Prometric/Models/IShape.cs
@@ -0,0 +1,23 @@
+﻿namespace Prometric.Models
+{
+    /// <summary>
+    ///     Interface for the shapes
+    /// </summary>
+    public interface IShape
+    {
+        /// <summary>
+        ///     Object name
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        ///     Area
+        /// </summary>
+        public double Area { get; }
+
+        /// <summary>
+        ///     Perimeter
+        /// </summary>
+        public string Perimeter { get; }
+    }
+}
